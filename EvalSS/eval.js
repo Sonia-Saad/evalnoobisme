38de6c22d67334ed3e8c82aa5a5fe26b3d71d6e5
Sonia-Saad <80457853+Sonia-Saad@users.noreply.github.com>
2021-04-07 20:45:08 +0200

    Add files via upload

---
+++ EvalSS/eval.js
@@ -0,0 +1,33 @@
+//foncrton change background image 
+var image1 = images("src" / Annexes / background.jpg);
+var image2 = images("src" / Annexes / background.jpg);
+
+function changeImage(image2) {
+    var image2 = $("/Annexes/background.jpg");
+    myimage.attr('src' /Annexes/traffic.png );
+
+    setTimeout(function () {
+
+        change_background('/Annexes/traffic.png');
+
+    }, 2000);
+
+    
+    
+    
+    
+    
+//     [
+//         url("/Annexes/background.jpg"), url("/Annexes/traffic.png")
+//     ],
+//         index = Math.floor(Math.random() * images.length);
+
+//     document.querySelector("header").style.backgroundImage = images[index];
+
+// }
+
+// setInterval(function () {
+
+//     changeImage();
+
+// }, 2000);
